04ad76a5dcbc7d3f75a8d02c6cbbd9ec43eec450
Kerim Dinçer <kerim_dincer@hotmail.com>
2021-01-23 16:57:40 +0300

    Add project files.

---
+++ H2G4D5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace H2G4D5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, String> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(34, "İstanbul");
+            myDictionary.Add(35, "İzmir");
+            myDictionary.Add(06, "Ankara");
+            myDictionary.Add(58, "Sivas");
+
+            Console.WriteLine(myDictionary.Length);
+
+            foreach (var item in myDictionary.MyDictionaryItems)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value);
+            }
+        }
+    }
+}
